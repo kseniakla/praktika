faa01c299acd87b59b24ca72803b799de47d8e64
kseniakla <165650883+kseniakla@users.noreply.github.com>
2024-04-05 12:18:57 +0300

    analyzer

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import model.CompositionItem;
+
 public class Main {
     public static void main(String[] args) {
+        CompositionItem Item1 = new CompositionItem("", 0);
         System.out.println("Hello world!");
     }
 }
---
+++ src/model/CompositionItem.java
@@ -0,0 +1,11 @@
+package model;
+
+public class CompositionItem {
+    public String name;
+    public double value;
+
+    public CompositionItem(String name, double value) {
+        this.name = name;
+        this.value = value;
+    }
+}
