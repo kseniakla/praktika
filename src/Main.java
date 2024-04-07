ba07dc74ce68bc6addf98e52a0af1602afe2f2ce
kseniakla <165650883+kseniakla@users.noreply.github.com>
2024-04-07 21:52:19 +0300

    analyzer

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import model.CompositionItem;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         CompositionItem Item1 = new CompositionItem("", 0);
-        System.out.println("Hello world!");
+        System.out.println("Hello world!" + new Date());
+
     }
 }
--- src/model/CompositionItem.java
+++ src/model/CompositionItem.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class CompositionItem {
     public String name;
     public double value;
@@ -7,5 +10,10 @@ public class CompositionItem {
     public CompositionItem(String name, double value) {
         this.name = name;
         this.value = value;
+    }
+    public void DeliveryDate() {
+        System.out.println(System.currentTimeMillis());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        System.out.println(simpleDateFormat.format(new Date()));
     }
 }
